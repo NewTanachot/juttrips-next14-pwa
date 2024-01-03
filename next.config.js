41a723ff7ee2d82e674ffd1c45c217802fae30bc
New_2000 <TanachotNew2543@gmail.com>
2024-01-04 01:10:58 +0700

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// Configuration object tells the next-pwa plugin 
+const withPWA = require("next-pwa")({
+    dest: "public", // Destination directory for the PWA files
+    register: false, // Register the PWA service worker
+  });
+  
+// Export the combined configuration for Next.js with PWA support
+module.exports = withPWA(nextConfig);
